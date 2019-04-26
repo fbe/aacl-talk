4a3d749c53c43848af7e5746a770c3445755857c
Felix Becker <git@felixbecker.name>
2019-04-26 11:14:16 +0200

    add hints

--- aacl-01-own-classloader/src/main/java/name/felixbecker/aacl/demo01/App.java
+++ aacl-01-own-classloader/src/main/java/name/felixbecker/aacl/demo01/App.java
@@ -10,13 +10,15 @@ public class App
     {
         System.out.println( "Hello World!" );
         OwnDelegatingClassLoader classLoader = new OwnDelegatingClassLoader();
+        OwnDelegatingClassLoader classLoader2 = new OwnDelegatingClassLoader();
         final Class<?> stringClass = classLoader.loadClass("java.lang.String");
         final Class<?> customClass = classLoader.loadClass("jax.Fun");
-
-
+        final Class<?> customClass2 = classLoader2.loadClass("jax2.Fun");
         System.out.println(customClass);
-
         System.out.println(stringClass.getClassLoader());
+        System.out.println(Class.forName("java.lang.String", true, classLoader).getClassLoader());
+
+
     }
     
     // Demo own classloader which loads class from resource
