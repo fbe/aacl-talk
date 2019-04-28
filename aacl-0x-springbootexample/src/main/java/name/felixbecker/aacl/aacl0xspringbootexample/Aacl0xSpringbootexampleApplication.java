22d6e56f5af325885771596ea96443dc3a21ac84
Felix Becker <git@felixbecker.name>
2019-04-28 14:16:17 +0200

    step - spring boot example

---
+++ aacl-0x-springbootexample/src/main/java/name/felixbecker/aacl/aacl0xspringbootexample/Aacl0xSpringbootexampleApplication.java
@@ -0,0 +1,13 @@
+package name.felixbecker.aacl.aacl0xspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aacl0xSpringbootexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aacl0xSpringbootexampleApplication.class, args);
+	}
+
+}
