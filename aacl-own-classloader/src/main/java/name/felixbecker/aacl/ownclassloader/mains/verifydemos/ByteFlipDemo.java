05835faabf520daacc1777fcf23d5e070c2f8ea5
Felix Becker <git@felixbecker.name>
2019-05-09 10:25:45 +0200

    remove non talk contents (verification demonstration)

--- aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/verifydemos/ByteFlipDemo.java
+++
@@ -1,8 +0,0 @@
-package name.felixbecker.aacl.ownclassloader.mains.verifydemos;
-
-
-public class ByteFlipDemo {
-
-    public static void main(String... args) throws Exception {
-    }
-}
--- aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/verifydemos/SelfInheritanceDemo.java
+++
@@ -1,9 +0,0 @@
-package name.felixbecker.aacl.ownclassloader.mains.verifydemos;
-
-import name.felixbecker.aacl.ownclassloader.classgenerator.BrokenClassGenerator;
-
-public class SelfInheritanceDemo {
-
-    public static void main(String... args) throws Exception {
-    }
-}
