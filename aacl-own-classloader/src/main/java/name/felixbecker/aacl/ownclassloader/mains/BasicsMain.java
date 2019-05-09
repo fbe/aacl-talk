ce3d0283851238f2d3699014266b8c5bd3e1b374
Felix Becker <git@felixbecker.name>
2019-05-09 15:19:21 +0200

    add class names for demo

--- aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/BasicsMain.java
+++ aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/BasicsMain.java
@@ -6,6 +6,9 @@ import java.net.URLClassLoader;
 
 public class BasicsMain
 {
+    final String testClassFQCN = "name.felixbecker.aacl.ownclassloader.model.TestClass";
+    final String driverClassFQCN = "java.sql.Driver";
+
     public static void main( String[] args ) throws Exception
     {
     }
