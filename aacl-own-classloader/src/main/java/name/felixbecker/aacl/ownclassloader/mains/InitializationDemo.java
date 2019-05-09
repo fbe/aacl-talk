856e2e3f699e84111be8fe8916e208d7fb7208c7
Felix Becker <git@felixbecker.name>
2019-05-09 15:21:25 +0200

    add testclassfqcn for init demo

--- aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/InitializationDemo.java
+++ aacl-own-classloader/src/main/java/name/felixbecker/aacl/ownclassloader/mains/InitializationDemo.java
@@ -2,6 +2,7 @@ package name.felixbecker.aacl.ownclassloader.mains;
 
 public class InitializationDemo {
 
+	final String testClassFQCN = "name.felixbecker.aacl.ownclassloader.model.TestClass";
 	public static void main(String... args) throws Exception {
 
 	}
